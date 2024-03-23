f495defdffbe96a85fa3e160b7fd109a5868b888
Jeff <jeffersonc.silva@hotmail.com>
2024-03-22 22:25:45 -0300

    feat: added bug fix for pagination

--- MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -34,7 +34,7 @@ namespace MagicVilla_VillaAPI.Controllers.v1
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")]int? occupancy,
-            [FromQuery]string? search, int pageSize = 3, int pageNumber = 1)
+            [FromQuery]string? search, int pageSize = 0, int pageNumber = 1)
         {
             try
             {
